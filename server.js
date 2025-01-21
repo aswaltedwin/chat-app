54e281798870d3ca21d40ef7d74c2ef711ec2be0
Aswalt Edwin <edwinaswalt06@gmail.com>
2025-01-21 10:34:24 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static('public'));
+// Track connected users
+const users = {};
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-    // Handle user joining
-    socket.on('userJoined', (userName) => {
-        socket.userName = userName;
-        io.emit('userJoined', userName);
+  // When a new user joins
+  socket.on("new-user", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("user-connected", username);
+  });
+
+  // When a message is sent
+  socket.on("chat-message", (data) => {
+    socket.broadcast.emit("chat-message", {
+      message: data.message,
+      username: users[socket.id],
+      time: new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
     });
+  });
 
-    // Handle chat messages
-    socket.on('chatMessage', ({ userName, message, time }) => {
-        // Emit the message to everyone except the sender
-        socket.broadcast.emit('chatMessage', { userName, message, time });
-    });
-
-    // Handle user disconnecting
-    socket.on('disconnect', () => {
-        if (socket.userName) {
-            io.emit('userLeft', socket.userName);
-        }
-    });
+  // When a user disconnects
+  socket.on("disconnect", () => {
+    const username = users[socket.id];
+    if (username) {
+      socket.broadcast.emit("user-disconnected", username);
+      delete users[socket.id];
+    }
+    console.log("A user disconnected");
+  });
 });
 
+// Use the environment-provided port or default to 3000
+const PORT = process.env.PORT || 3000;
+
+// Start the server
 server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
